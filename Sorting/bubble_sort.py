fb95468b4dc72f0a7c5203ff12b95f36c8a85afe
shiv santosh <shivsantosh04@gmail.com>
2025-11-26 16:23:06 +0530

    bubble sort added

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+print(bubble_sort([2,1,5,7,3,6,8]))
