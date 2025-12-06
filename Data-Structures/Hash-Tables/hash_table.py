6eb97902262b0ffef01ba7c1ca5d5aad6c9bc85d
Shiv <shivsantosh04@gmail.com>
2025-12-06 15:28:38 +0530

    learnt basics of hash table

---
+++ Data-Structures/Hash-Tables/hash_table.py
@@ -0,0 +1,12 @@
+#implementing hash tables using dictionaries in python
+#create an empty dictionary
+book = {}
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["avocado"] = 1.49
+print(book)
+
+#accessing price of an element
+print(book["apple"]) #output: 0.67
+print(book["milk"]) #output: 1.49
+print(book["avocado"]) #output: 1.49
